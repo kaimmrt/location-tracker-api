4f447e8eb0a271b31cd1d20f2ac62006bf8448a7
Mert Kaim <kaim.mrt@gmail.com>
2025-10-01 22:24:29 +0300

    area service update

--- src/modules/areas/areas.service.ts
+++ src/modules/areas/areas.service.ts
@@ -6,11 +6,11 @@ import { AreaRepository } from '../../repositories/area.repository';
 export class AreasService {
   constructor(private readonly areaRepo: AreaRepository) {}
 
-  async create(dto: CreateAreaDto) {
+  public async create(dto: CreateAreaDto) {
     return this.areaRepo.createWithPolygon(dto.name, dto.polygon);
   }
 
-  async findAll() {
+  public async findAll() {
     return this.areaRepo.findAll();
   }
 }
